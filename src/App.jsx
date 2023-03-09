e49533800a149dfbaf653b47b054fb2d424b8ec2
DarkxDev <bryanfernandohurtado@gmail.com>
2023-03-09 10:57:27 -0500

    Set up Hero and Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import Loader from "./components/Loader";
+
 const App = () => {
   return (
-    <div className="App">
-      <div>3D Darkx's Portfolio</div>
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
